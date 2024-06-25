25f1d60a2f34d534d506a5a2dcb930400fb32bf9
dsbiztiu22150152 <166495942+dsbiztiu22150152@users.noreply.github.com>
2024-06-25 18:17:48 +0900

    add file

---
+++ Yune/R/hello.R
@@ -0,0 +1,24 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+#' @title こんちは
+#' @description こんちは表示・グラフを表示
+#' @details 詳細不明
+#' @import ggplot2
+#' @export hello
+
+hello <- function() {
+  print("konchiwa, world!")
+}
